c51aac94c61379b9c3868b1b4bb5e92e912be57d
richard <rcs62@cam.ac.uk>
2020-04-02 00:10:01 +0100

    Added graph looking at repayment vs a given parameter

--- student-loan-calculator/index.js
+++ student-loan-calculator/index.js
@@ -1,210 +1,309 @@
-    function salary(year) {
-        return 30000 * (1.05 ** year)
-    }
+function salary(year) {
+    return 30000 * (1.05 ** year)
+}
 
-    function DebugInfo(loanAmount, salary, interest, repayment) {
-        this.loanAmount = loanAmount;
-        this.salary = salary;
-        this.interest = interest;
-        this.repayment = repayment;
-    }
+function DebugInfo(loanAmount, salary, interest, repayment) {
+    this.loanAmount = loanAmount;
+    this.salary = salary;
+    this.interest = interest;
+    this.repayment = repayment;
+}
 
-    function runSimulation(loanAmount, salary, repaymentPercentage, initialPayoff) {
-        let debugStr = "";
-        let allData = [];
-        let changeOrder = false;
-        let balanceLog = [];
-        let interestTotal = 0;
-        let repaymentTotal = 0;
+function runSimulation(loanAmount, salary, repaymentPercentage, initialPayoff) {
+    let debugStr = "";
+    let allData = [];
+    let changeOrder = false;
+    let balanceLog = [];
+    let interestTotal = 0;
+    let repaymentTotal = 0;
 
-        loanAmount -= initialPayoff;
-
-        for (let year = 0; year < 30; year++) {
-            debugStr += "Year " + year.toString() + ": " + loanAmount.toString() + "\n";
-
-            let currentSalary = salary(year);
-
-            let prevLoanAmount = loanAmount;
-            let yearInterestTotal = 0;
-            let yearPaymentTotal = 0;
-            for (let month = 0; month < 12; month++) {
-                let currentInterest, currentRepayment;
-                if (changeOrder) {
-                    currentInterest = interest(currentSalary, loanAmount);
-                    currentRepayment = repayment(currentSalary, loanAmount, repaymentPercentage);
-                    balanceLog.push(loanAmount);
-                    loanAmount = loanAmount + currentInterest - currentRepayment;
-                } else {
-                    currentRepayment = repayment(currentSalary, loanAmount, repaymentPercentage);
-                    balanceLog.push(loanAmount);
-                    loanAmount -= currentRepayment;
-                    currentInterest = interest(currentSalary, loanAmount);
-                    loanAmount += currentInterest;
-                }
-                interestTotal += currentInterest;
-                yearInterestTotal += currentInterest;
-                repaymentTotal += currentRepayment;
-                yearPaymentTotal += currentRepayment;
-            }
-
-            allData.push(new DebugInfo(prevLoanAmount, currentSalary, yearInterestTotal, yearPaymentTotal))
-        }
-
-        return {"balanceLog": balanceLog, "interestTotal": interestTotal, "repaymentTotal": repaymentTotal + Number(initialPayoff)}
-    }
-
-    function interest(salary, balance) {
-        let additional = 0.03;
-        let rpi = 0.024;
-        let interest = 0;
-
-        if (salary < 25725) {
-            interest = rpi * balance;
-        } else if (salary > 46305) {
-            interest = (rpi + additional) * balance;
-        } else {
-            let fraction = (salary - 25725) / (46305 - 25725);
-            interest = (rpi + fraction * additional) * balance;
-        }
-
-        return interest / 12;
-    }
-
-    function repayment(salary, balance, paymentPercentage) {
-        let threshold = 25725;
-        // let paymentPercentage = 0.09;
-
-        if (salary < threshold) {
-            return 0
-        } else {
-            repayAmount = paymentPercentage * (salary - threshold)
-        }
-
-        return Math.min(balance, repayAmount / 12)
-    }
-
-    let labels = [];
+    loanAmount -= initialPayoff;
 
     for (let year = 0; year < 30; year++) {
+        debugStr += "Year " + year.toString() + ": " + loanAmount.toString() + "\n";
+
+        let currentSalary = salary(year);
+
+        let prevLoanAmount = loanAmount;
+        let yearInterestTotal = 0;
+        let yearPaymentTotal = 0;
         for (let month = 0; month < 12; month++) {
-            labels.push(year.toString());
+            let currentInterest, currentRepayment;
+            if (changeOrder) {
+                currentInterest = interest(currentSalary, loanAmount);
+                currentRepayment = repayment(currentSalary, loanAmount, repaymentPercentage);
+                balanceLog.push(loanAmount);
+                loanAmount = loanAmount + currentInterest - currentRepayment;
+            } else {
+                currentRepayment = repayment(currentSalary, loanAmount, repaymentPercentage);
+                balanceLog.push(loanAmount);
+                loanAmount -= currentRepayment;
+                currentInterest = interest(currentSalary, loanAmount);
+                loanAmount += currentInterest;
+            }
+            interestTotal += currentInterest;
+            yearInterestTotal += currentInterest;
+            repaymentTotal += currentRepayment;
+            yearPaymentTotal += currentRepayment;
         }
+
+        allData.push(new DebugInfo(prevLoanAmount, currentSalary, yearInterestTotal, yearPaymentTotal))
     }
 
-    var ctx = document.getElementById('loanChart').getContext('2d');
-    var data = {
-        labels: labels,
-        datasets: [
-            {
-                label: "Loan balance",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29],
-                lineTension: 0
-            }
-        ]
-    };
+    return {
+        "balanceLog": balanceLog,
+        "interestTotal": interestTotal,
+        "repaymentTotal": repaymentTotal + Number(initialPayoff)
+    }
+}
 
-    var options = {
-        lineTension: 0,
-        maintainAspectRatio: false,
-        scales: {
-            xAxes: [{
-                ticks: {
-                    min: 3,
-                    max: 10,
-                    maxTicks: 10,
-                    maxTicksLimit: 30
-                },
-                scaleLabel: {
-                    display: true,
-                    labelString: "Year"
-                }
-            }],
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true,
-                    callback: function(value, index, values) {
-                        return '£' + value;
-                    }
-                },
-                scaleLabel: {
-                    display: true,
-                    labelString: "Loan amount"
-                }
-            }]
-        },
-        tooltips: {
-            callbacks: {
-                label: function(tooltipItem, data) {
-                    var label = data.datasets[tooltipItem.datasetIndex].label || '';
+function interest(salary, balance) {
+    let additional = 0.03;
+    let rpi = 0.024;
+    let interest = 0;
 
-                    if (label) {
-                        label += ': ';
-                    }
-                    label += Math.round(tooltipItem.yLabel/10) * 10;
-                    return label;
-                }
-            }
-        }
-    };
-
-    var lineChart = new Chart(ctx, {
-        type: "line",
-        data: data,
-        options: options
-    });
-
-    function updateMainChart() {
-        let tempSal = function(year) { return eval(document.getElementById("salaryFormula").value) };
-        test = runSimulation(document.getElementById("loanAmount").value, tempSal, document.getElementById("perYearPayoff").value/100, document.getElementById("initialPayoff").value);
-        lineChart.data.datasets[0].data = test["balanceLog"];
-        lineChart.update();
-        document.getElementById("totalRepaymentInfo").innerText = Math.round(test["repaymentTotal"]).toString();
-        document.getElementById("totalInterestInfo").innerText = Math.round(test["interestTotal"]).toString();
-        document.getElementById("totalRemainingInfo").innerText = Math.round(test["balanceLog"][test["balanceLog"].length - 1]).toString();
-        if(test["balanceLog"][test["balanceLog"].length - 1] < 1) {
-            document.getElementById("controls1").style.background = "seagreen";
-        } else {
-            document.getElementById("controls1").style.background = "";
-        }
+    if (salary < 25725) {
+        interest = rpi * balance;
+    } else if (salary > 46305) {
+        interest = (rpi + additional) * balance;
+    } else {
+        let fraction = (salary - 25725) / (46305 - 25725);
+        interest = (rpi + fraction * additional) * balance;
     }
 
-    updateMainChart();
+    return interest / 12;
+}
 
-    let toBePayed = 60000;
+function repayment(salary, balance, paymentPercentage) {
+    let threshold = 25725;
+    // let paymentPercentage = 0.09;
+
+    if (salary < threshold) {
+        return 0
+    } else {
+        repayAmount = paymentPercentage * (salary - threshold)
+    }
+
+    return Math.min(balance, repayAmount / 12)
+}
+
+let labels = [];
+
+for (let year = 0; year < 30; year++) {
+    for (let month = 0; month < 12; month++) {
+        labels.push(year.toString());
+    }
+}
+
+var ctx = document.getElementById('loanChart').getContext('2d');
+var data = {
+    labels: labels,
+    datasets: [
+        {
+            label: "Loan balance",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29],
+            lineTension: 0
+        }
+    ]
+};
+
+var options = {
+    lineTension: 0,
+    maintainAspectRatio: false,
+    scales: {
+        xAxes: [{
+            ticks: {
+                min: 3,
+                max: 10,
+                maxTicks: 10,
+                maxTicksLimit: 30
+            },
+            scaleLabel: {
+                display: true,
+                labelString: "Year"
+            }
+        }],
+        yAxes: [{
+            ticks: {
+                beginAtZero: true,
+                callback: function (value, index, values) {
+                    return '£' + value;
+                }
+            },
+            scaleLabel: {
+                display: true,
+                labelString: "Loan amount"
+            }
+        }]
+    },
+    tooltips: {
+        callbacks: {
+            label: function (tooltipItem, data) {
+                var label = data.datasets[tooltipItem.datasetIndex].label || '';
+
+                if (label) {
+                    label += ': ';
+                }
+                label += Math.round(tooltipItem.yLabel / 10) * 10;
+                return label;
+            }
+        }
+    }
+};
+
+var lineChart = new Chart(ctx, {
+    type: "line",
+    data: data,
+    options: options
+});
+
+function updateMainChart() {
+    let tempSal = function (year) {
+        return eval(document.getElementById("salaryFormula").value)
+    };
+    test = runSimulation(document.getElementById("loanAmount").value, tempSal, document.getElementById("perYearPayoff").value / 100, document.getElementById("initialPayoff").value);
+    lineChart.data.datasets[0].data = test["balanceLog"];
+    lineChart.update();
+    document.getElementById("totalRepaymentInfo").innerText = Math.round(test["repaymentTotal"]).toString();
+    document.getElementById("totalInterestInfo").innerText = Math.round(test["interestTotal"]).toString();
+    document.getElementById("totalRemainingInfo").innerText = Math.round(test["balanceLog"][test["balanceLog"].length - 1]).toString();
+    if (test["balanceLog"][test["balanceLog"].length - 1] < 1) {
+        document.getElementById("controls1").style.background = "seagreen";
+    } else {
+        document.getElementById("controls1").style.background = "";
+    }
+}
+
+updateMainChart();
+
+var ctx1 = document.getElementById('repaymentChart').getContext('2d');
+var data1 = {
+    labels: [0, 1000, 2000],
+    datasets: [
+        {
+            label: "Total repayment amount",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [1, 1, 1],
+            lineTension: 0
+        }
+    ]
+};
+
+var options = {
+    lineTension: 0,
+    maintainAspectRatio: false,
+    scales: {
+        xAxes: [{
+            ticks: {
+                min: 3,
+                max: 10,
+                maxTicks: 10,
+                maxTicksLimit: 30
+            },
+            scaleLabel: {
+                display: true,
+                labelString: "Year"
+            }
+        }],
+        yAxes: [{
+            ticks: {
+                beginAtZero: true,
+                callback: function (value, index, values) {
+                    return '£' + value;
+                }
+            },
+            scaleLabel: {
+                display: true,
+                labelString: "Loan amount"
+            }
+        }]
+    },
+    tooltips: {
+        callbacks: {
+            label: function (tooltipItem, data) {
+                var label = data.datasets[tooltipItem.datasetIndex].label || '';
+
+                if (label) {
+                    label += ': ';
+                }
+                label += Math.round(tooltipItem.yLabel / 10) * 10;
+                return label;
+            }
+        }
+    }
+};
+
+var lineChart1 = new Chart(ctx1, {
+    type: "line",
+    data: data1,
+    options: options
+});
+
+function updateChart2() {
+    let tempSal = new Function("year", "return " + document.getElementById("salaryFormula").value);
+    let toBePayed = document.getElementById("loanAmount2").value;
+    let initialPayoff = document.getElementById("initialPayoff2").value;
+    let repaymentPercentage = document.getElementById("perYearPayoff2").value / 100;
     let resultsArray = [];
     let resultsLabels = [];
-    for (let initalPayment = 0; initalPayment < 60000; initalPayment += 1000) {
-        let results = runSimulation(toBePayed - initalPayment, salary)
-        resultsArray.push(results["repaymentTotal"] + initalPayment)
-        resultsLabels.push(initalPayment)
+
+    let selection = document.getElementById("varyingParameter").value;
+    console.log(selection);
+    if (selection === "initialPayoff") {
+        document.getElementById("perYearPayoffControls").style.display = "";
+        document.getElementById("initialPayoffControls").style.display = "none";
+
+        for (let initialPayment = 0; initialPayment < 60000; initialPayment += 1000) {
+            let results = runSimulation(toBePayed, tempSal, repaymentPercentage, initialPayment);
+            resultsArray.push(results["repaymentTotal"]);
+            resultsLabels.push("£" + initialPayment.toString())
+        }
+    } else if (selection === "percentagePayoff") {
+        document.getElementById("perYearPayoffControls").style.display = "none";
+        document.getElementById("initialPayoffControls").style.display = "";
+
+        for (let repaymentPercentageV = 0.09; repaymentPercentageV < 1; repaymentPercentageV += 0.01) {
+            let results = runSimulation(toBePayed, tempSal, repaymentPercentageV, initialPayoff);
+            resultsArray.push(results["repaymentTotal"]);
+            resultsLabels.push(Math.round(repaymentPercentageV * 100).toString() + "%")
+        }
     }
 
-    var ctx1 = document.getElementById('repaymentChart').getContext('2d');
-    var data1 = {
-        labels: resultsLabels,
-        datasets: [
-            {
-                label: "Total repayment amount",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: resultsArray,
-                lineTension: 0
-            }
-        ]
-    };
+    let globalMin = -1;
+    let globalMinValue = 9999999999999;
+    for(let i = 0; i < resultsArray.length; i++) {
+        if(globalMinValue > resultsArray[i]) {
+            globalMin = i;
+            globalMinValue = resultsArray[i];
+        }
+    }
 
-    var lineChart1 = new Chart(ctx1, {
-        type: "line",
-        data: data1,
-        options: options
-    });
+    if (selection === "initialPayoff") {
+        document.getElementById("optimumValueInfo").innerHTML = "Initial payoff of " + resultsLabels[globalMin].toString() + "<br>Results in £" + globalMinValue.toString() + " payment";
+    } else if (selection === "percentagePayoff") {
+        document.getElementById("optimumValueInfo").innerHTML = "Paying off " + resultsLabels[globalMin].toString() + " of your salary<br>Results in £" + Math.round(globalMinValue).toString() + " payment";
+    }
+
+    lineChart1.data.labels = resultsLabels;
+    lineChart1.data.datasets[0].data = resultsArray;
+    lineChart1.update()
+}
+
+let formMembers = document.getElementById("controls2").getElementsByTagName("*");
+for (let i = 0; i < formMembers.length; i++) {
+    let el = formMembers[i];
+    el.addEventListener("input", updateChart2);
+}
+
+updateChart2();
